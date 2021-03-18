24c704def6a524753d39bba2d783b17db9a73330
Jason Adam <22adamj22@gmail.com>
2021-03-17 21:48:06 -0700

    updated README and added samples

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# text-similarity
-Microservice to detect similarity between two input strings
+# Text Similarity Service  
+Microservice to detect similarity between two input strings using [Levenschtein distance](https://en.wikipedia.org/wiki/Levenshtein_distance).  
+
+## Run The Application  
+Below are instructions for ways to run the application.  
+
+### Locally (no Docker)  
+Run the following command:  
+
+```bash  
+make run
+```
+
+This will start the application. You can open another terminal window and run the following:  
+
+```bash  
+sh run_samples.sh
+```
--- internal/api/services/text_similarity.go
+++ internal/api/services/text_similarity.go
@@ -1,6 +1,8 @@
 package services
 
-import "github.com/jason-adam/text-similarity/internal/api/models"
+import (
+	"github.com/jason-adam/text-similarity/internal/api/models"
+)
 
 // SimilarityService calculates the similarity between two text entries
 type SimilarityService struct{}
@@ -12,8 +14,59 @@ func NewSimilarityService() *SimilarityService {
 
 // Calculate returns the similarity score
 func (s *SimilarityService) Calculate(t *models.TextPair) (*models.SimilarityScore, error) {
-	// TODO: implement levenschtein distance
+
+	edits := make([][]int, len(t.SecondText)+1)
+	for y := range edits {
+		edits[y] = make([]int, len(t.FirstText)+1)
+		for x := range edits[y] {
+			edits[y][x] = x
+		}
+	}
+
+	for i := 1; i < len(t.SecondText)+1; i++ {
+		edits[i][0] = edits[i-1][0] + 1
+	}
+
+	for i := 1; i < len(t.SecondText)+1; i++ {
+		for j := 1; j < len(t.FirstText)+1; j++ {
+			if t.SecondText[i-1] == t.FirstText[j-1] {
+				edits[i][j] = edits[i-1][j-1]
+			} else {
+				edits[i][j] = 1 + min(edits[i-1][j-1], edits[i-1][j], edits[i][j-1])
+			}
+		}
+	}
+
+	score := edits[len(t.SecondText)][len(t.FirstText)]
+	normalizedScore := normalize(score, t)
+
 	return &models.SimilarityScore{
-		Score: 0.5,
+		Score: normalizedScore,
 	}, nil
+}
+
+func min(args ...int) int {
+	curr := args[0]
+	for _, num := range args {
+		if curr > num {
+			curr = num
+		}
+	}
+	return curr
+}
+
+func max(args ...int) int {
+	curr := args[0]
+	for _, num := range args {
+		if curr < num {
+			curr = num
+		}
+	}
+	return curr
+}
+
+func normalize(s int, t *models.TextPair) float32 {
+	maxDistance := max(len(t.FirstText), len(t.SecondText))
+	normalizedScore := 1.0 - (float32(s) / float32(maxDistance))
+	return normalizedScore
 }

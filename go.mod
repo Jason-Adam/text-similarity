49ed426c13a78f9ba0b16d326b86a56b8e0f7053
Jason Adam <22adamj22@gmail.com>
2021-03-18 20:16:54 -0700

    added simple ping router test

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.4.0
 	golang.org/x/sys v0.0.0-20210317225723-c4fcb01b228e // indirect
 	golang.org/x/text v0.3.3 // indirect
 )
---
+++ internal/api/router/router_test.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPingRoute(t *testing.T) {
+	router := Setup()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/ping", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "{\"message\":\"pong\"}", w.Body.String())
+}
